ef4c3fa64527861bae71f6a988576b2a865439d8
devfernando <fernandorufinoalves@gmail.com>
2023-10-20 20:42:26 -0300

    criando HTML com JS

--- Favorites.js
+++ Favorites.js
@@ -1,6 +1,24 @@
 export class Favorites {
   constructor(root) {
     this.root = document.querySelector(root)
+    this.load()
+  }
+
+  load() {
+    this.entries = [
+      {
+        login: 'fernandoalvesrufino',
+        name: "Fernando Alves Rufino",
+        public_repos: '76',
+        followers: '120000'
+      },
+      {
+        login: 'maykbrito',
+        name: "Mayk Brito",
+        public_repos: '76',
+        followers: '120000'
+      }
+    ]
   }
 }
 
@@ -8,19 +26,52 @@ export class FavoritesView extends Favorites {
   constructor(root) {
     super(root)
 
+    this.tbody = this.root.querySelector('table tbody')
+
     this.update()
   }
 
   update() {
     this.removeAllTr()
 
+    this.entries.forEach( user => {
+      const row = this.createRow()
+
+      row.querySelector('.user img').src = `https://github.com/${user.login}.png`
+      row.querySelector('.user img').alt = `Imagem de ${user.name}`
+      row.querySelector('.user p').textContent = user.name
+      row.querySelector('.user span').textContent = user.login
+      row.querySelector('.repositories').textContent = user.public_repos
+      row.querySelector('.followers').textContent = user.followers
+
+
+
+      this.tbody.append(row)
+    })
 
   }
 
-  removeAllTr() {
-    const tbody = this.root.querySelector('table tbody')
+  createRow() {
+    const tr = document.createElement('tr')
 
-    tbody.querySelectorAll('tr').forEach((tr) => {
+    tr.innerHTML = `
+      <td class="user">
+        <img src="https://github.com/fernandoalvesrufino.png" alt="user">
+        <a href="https://github.com/fernandoalvesrufino">
+          <p>Fernando Alves Rufino</p>
+          <span>/fernandorufinoalves</span>
+        </a>
+      </td>
+      <td class="repositories">123</td>
+      <td class="followers">123</td>
+      <td class="remove">Remover</td>
+    `
+
+    return tr
+  }
+
+  removeAllTr() {
+    this.tbody.querySelectorAll('tr').forEach((tr) => {
       tr.remove()
     });
   }
